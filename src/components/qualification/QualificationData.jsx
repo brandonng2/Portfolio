6686dbe01cad6896a50c2aa36920fafe8d8c8974
brandonng2 <brandonng0904@gmail.com>
2026-01-05 13:03:54 -0800

    2026 update

--- src/components/qualification/QualificationData.jsx
+++ src/components/qualification/QualificationData.jsx
@@ -22,32 +22,44 @@ export const EducationData = [
 export const ExperienceData = [
     {
         id: 1,
-        title: "Frontend & Backend Developer",
-        subtitle: "Tech4Good Lab",
-        date: "Sept 2022 - Jun 2023"
-    },
-    {
-        id: 2,
         title: "Backend Developer Team Lead",
         subtitle: "Tech4Good Lab",
         date: "Jun 2023 - Sept 2023"
     },
     {
-        id: 3,
+        id: 2,
         title: "Full Stack SWE Intern",
         subtitle: "CIP4Gov",
         date: "Jun 2024 - Sept 2024"
     },
     {
-        id: 4,
+        id: 3,
         title: "Software Engineer Intern",
         subtitle: "CDPH",
         date: "Feb 2025 - Jun 2025"
+    },
+    {
+        id: 4,
+        title: "Software Engineer Intern",
+        subtitle: "Comcast",
+        date: "Jun 2025 - Aug 2025"
     },
     {
         id: 5,
         title: "Software Engineer Intern",
         subtitle: "Comcast",
         date: "Jun 2025 - Aug 2025"
+    },
+    {
+        id: 6,
+        title: "Machine Learning Research Assistant",
+        subtitle: "UCSD - XR Lab",
+        date: "Sept 2025 - Current"
+    },
+    {
+        id: 7,
+        title: "Machine Learning Apprenticeship",
+        subtitle: "UCSD - MOSAIC Lab",
+        date: "Sept 2025 - Current"
     }
 ];
